ba12b3677f554316127fb5ebbfe780feb1893201
Dana Lark <danalark0109@gmail.com>
2021-07-06 13:43:41 +1000

    added Sass and some sliders with state

--- public/app.js
+++ public/app.js
@@ -18,5 +18,15 @@ const geoSuccess = position => {
     fetchWeather([lat, lon]).then(res => results(res.data.timelines[0].intervals[0].values));
 }
 
+const changeText = (e) => {
+    console.log(e);
+    const sliderVal = e.target.value;
+    const valueNode = document.querySelector(`#${e.target.id}Value`)
+    valueNode.innerText = sliderVal;
+}
+// slider on change update text
+document.querySelector('#temperature').addEventListener('change', e => changeText(e));
+document.querySelector('#windSpeed').addEventListener('change', e => changeText(e));
+// add event listener to get weather button
 const weatherButton = document.querySelector('#weatherBtn');
 weatherButton.addEventListener('click', handleClick);
